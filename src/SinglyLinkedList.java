b952b0af7d91debb2c04cc3874e7a27a742cf83e
vivek84527 <viveksingh84527@gmail.com>
2021-02-24 11:01:44 +0530

    Delete From the first Node

--- src/SinglyLinkedList.java
+++ src/SinglyLinkedList.java
@@ -1,3 +1,5 @@
+import javafx.scene.control.skin.SliderSkin;
+
 import java.util.LinkedList;
 
 public class SinglyLinkedList {
@@ -59,7 +61,94 @@ public class SinglyLinkedList {
         temp.next=null;
         return temp;
     }
+    public  ListNode deleteLast(){
+        if(head==null||head.next==null){
+            return head;
+        }
+        ListNode current=head;
+        ListNode previous=null;
+        while(current.next!=null){
+            previous=current;
+            current=current.next;
 
+        }
+        previous.next=null;
+        return current;
+    }
+
+    public void insertPosition(int position,int value){
+        ListNode node=new ListNode(value);
+        if(position==1){
+            node.next=head;head=node;
+        }
+        else{
+            ListNode previous=head;
+            int count=1;
+            while(count<position-1){
+                previous=previous.next;
+                count++;
+            }
+            ListNode current=previous.next;
+            previous.next=node;
+            node.next=current;
+
+        }
+
+    }
+    public void deletePostion(int postion){
+        if(postion==1){
+            head=head.next;
+        }
+        else {
+            ListNode previous=head;
+            int count=1;
+            while (count<postion-1){
+                previous=previous.next;
+                count++;
+            }
+            ListNode current =previous.next;
+            previous.next=current.next;
+        }
+    }
+    public boolean find(int searchKey){
+        if(head==null){
+            return false;
+        }
+        ListNode current=head;
+        while(current!=null){
+            if(current.data==searchKey){
+                return true;
+            }
+            current=current.next;
+        }
+        return false;
+    }
+    public ListNode reverse(){
+        ListNode current=head;
+        ListNode previous=null;
+        ListNode next= null;
+        while(current!=null){
+            next=current.next;
+            current.next=previous;
+            previous=current;
+            current=next;
+
+
+        }
+        return previous;
+    }
+    public ListNode getMiddleNode(){
+        if(head==null){
+            return null;
+        }
+        ListNode slowPtr=head;
+        ListNode fastptr=head;
+        while(fastptr!=null&&fastptr.next!=null){
+            slowPtr=slowPtr.next;
+            fastptr=fastptr.next.next;
+        }
+        return slowPtr;
+    }
     public static void main(String[] args) {
         SinglyLinkedList sll=new SinglyLinkedList();
         sll.head=new ListNode(10);
@@ -73,8 +162,22 @@ public class SinglyLinkedList {
         sll.insertLast(25);
         sll.deleteFirst();
         sll.Display();
+        sll.deleteLast();
+        sll.Display();
+        sll.insertPosition(3,20);
+        sll.Display();
         System.out.println(sll.Length());
+        boolean f=sll.find(11);
+        if(f){
+            System.out.println("Search key find!!!");
+        }
+        else{
+            System.out.println("Search key Not Find!!!");
+        }
+        System.out.println(sll.reverse().data);
+        System.out.println(sll.getMiddleNode().data);
 
 
     }
+
 }
