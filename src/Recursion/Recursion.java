40715d573196af5dd665c2ac01ae7c84d5a14adf
vivek84527 <viveksingh84527@gmail.com>
2021-04-09 11:22:39 +0530

    Recursion

--- src/Recursion/Recursion.java
+++ src/Recursion/Recursion.java
@@ -1,4 +1,28 @@
 package Recursion;
 
 public class Recursion {
+//    O(1)
+    int sum(int n){
+        return n*(n+1)/2;
+    }
+
+//    O(n)
+    int sum1(int n){
+        int i,s=0;
+        for (int i1 = 0; i1 <= n; i1++) {
+            s=s+i1;
+
+        }
+        return s;
+    }
+
+//    Recursion
+    int sum2(int n){
+        if(n==0){
+            return 0;
+        }
+        else {
+            return sum2(n-1)+n;
+        }
+    }
 }
