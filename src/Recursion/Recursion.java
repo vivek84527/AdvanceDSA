3ce2f416ab56a2f46101daa0bf31ac90388c230e
vivek84527 <viveksingh84527@gmail.com>
2021-04-09 11:34:04 +0530

    Factorial

--- src/Recursion/Recursion.java
+++ src/Recursion/Recursion.java
@@ -25,4 +25,13 @@ public class Recursion {
             return sum2(n-1)+n;
         }
     }
+//    Factorial
+    int fact(int n){
+        if(n==0){
+            return 1;
+
+        }
+        else
+            return fact(n-1)*n;
+    }
 }
